cefdf85578af588eeb8c9ca10319b24ae7cd82f5
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-05-09 13:20:33 +0530

    completed

--- src/components/LapTimer.js
+++ src/components/LapTimer.js
@@ -1,16 +1,19 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import LapTimer from "./components/LapTimer";
+import react from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [laps, setLaps] = useState([]);
+const LapTimer = () => {
+  // Variables
+  const [time, setTime] = useState(0); //To store the state of time
+  const [isRunning, setIsRunning] = useState(false); //Store the state of the timer, whether it is running or not
+  const [laps, setLaps] = useState([]); //Store all the laps
 
   const timeInHours = Math.floor(time / 360000);
   const timeInMinutes = Math.floor((time % 360000) / 6000);
   const timeInSeconds = Math.floor((time % 6000) / 100);
   const timeInMilliseconds = Math.floor(time % 100);
+
+  // Method for timer
 
   const addLap = () => {
     const newLap =
@@ -21,10 +24,9 @@ function App() {
       timeInSeconds +
       ":" +
       timeInMilliseconds;
-    console.log(newLap);
+
     setLaps([...laps, newLap]);
   };
-  console.log(laps, "Laps");
 
   useEffect(() => {
     let intervalId = null;
@@ -33,27 +35,26 @@ function App() {
         setTime((prevTime) => prevTime + 1);
       });
     }
+
     return () => {
       clearInterval(intervalId);
     };
   }, [isRunning]);
-
-  // Start timer
+  // Operations on timer like: start, stop, lap & reset
+  //   Start timer
   const startTimer = () => {
     setIsRunning(true);
   };
-
   // Stop timer
   const stopTimer = () => {
     setIsRunning(false);
   };
-
-  // Lap timer
+  //   lap timer
   const lapTimer = () => {
     addLap();
   };
 
-  // Reset timer
+  //   const reset timer
   const resetTimer = () => {
     setIsRunning(false);
     setTime(0);
@@ -61,29 +62,28 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <LapTimer />
-      {/* <h1>Lap Timer</h1>
+    <div>
+      <h1>Lap Timer</h1>
       <p>
-        {timeInHours}:{timeInMinutes}:{timeInSeconds}:{timeInMilliseconds}{" "}
+        {timeInHours}:{timeInMinutes}:{timeInSeconds}:{timeInMilliseconds}
       </p>
-      <span>
+      <div>
         <button onClick={startTimer}>Start Timer</button>
         <button onClick={stopTimer}>Stop Timer</button>
-        <button onClick={lapTimer}>Lap Timer</button>
-        <button onClick={resetTimer}>Reset Timer</button>
-      </span>
-
-      {laps.length > 0 &&
-        laps.map((lap) => {
+        <button onClick={lapTimer}>Lap Time</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+      <ul>
+        {laps.map((lap) => {
           return (
             <div>
               <li>{lap}</li>
             </div>
           );
-        })} */}
+        })}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default LapTimer;
